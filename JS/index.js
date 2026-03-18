bd6c9159bc6152c691c5139e9bd26e875cf47ead
Swati Singh <swatisingh000beg@gmail.com>
2026-03-18 12:03:43 +0530

    changes

---
+++ JS/index.js
@@ -0,0 +1,41 @@
+// console.log("Hello world")
+
+// let city = "bhopal";
+// city = "Indore";  //allowed
+// console.log("city",city);
+
+// const country = "India";
+// country = "USA";
+// console.log("country",country);
+
+// let marks = 90;
+// let student = "Rahul";
+// let isPassed = true;
+// let x;
+// let y = null;
+
+// console.log("marks,student,ispassed,x,y---->",marks,student,isPassed,x,y);
+
+// let student = "Rahul";
+// let x = 5;
+// let z=typeof x;
+// typeof student;
+
+// console.log("z-->",z);
+
+// let a =10;
+// let b = 3.5;
+
+// let inf = Infinity;
+// let notANumber = NaN;
+
+// console.log(typeof notANumber);
+
+let age = "20";
+Number(age);
+parseInt("10.5");
+
+console.log(typeof(parseInt("10.5")));
+
+
+console.log("age-->",typeof Number(age));
