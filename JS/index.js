eb65131c811db7210d15406c7672a5d376a845e2
Swati Singh <swatisingh000beg@gmail.com>
2026-03-18 12:43:16 +0530

    changes

--- JS/index.js
+++ JS/index.js
@@ -31,11 +31,52 @@
 
 // console.log(typeof notANumber);
 
-let age = "20";
-Number(age);
-parseInt("10.5");
+// let age = "20";
+// Number(age);
+// parseInt("10.5");
 
-console.log(typeof(parseInt("10.5")));
+// console.log(typeof(parseInt("10.5")));
 
 
-console.log("age-->",typeof Number(age));
+// console.log("age-->",typeof Number(age));
+
+//strings
+
+// let firstName = "Swati";
+// let course = 'javascript';
+
+// firstName.length
+// firstName.toUpperCase()
+// firstName.toLowerCase()
+// firstName.includes("Swa")
+
+// console.log(firstName.includes("java"));
+
+// let first = "Swati";
+// let last = "Kumari";
+
+// let full = first + " " + last;
+// console.log(full);
+
+// Tempelate literals
+
+// let firstName = "Swati";
+// let age = 21;
+
+// let msg = `My name is ${firstName} and my age is ${age};`
+
+// console.log("message",msg);
+
+// let a = 10, b = 20;
+// let Sum = `Sum is ${a+b}`;
+
+// console.log("Sum", Sum);
+
+//NAN
+
+// let a = 10;
+// let b = "abc";
+
+// let result = a*b;
+// console.log(typeof result);
+
