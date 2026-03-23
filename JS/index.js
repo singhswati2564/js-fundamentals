4c07bb93a30fbb00316f4034be5b79e9232c17df
Swati Singh <swatisingh000beg@gmail.com>
2026-03-23 10:47:02 +0530

    index.js

--- JS/index.js
+++ JS/index.js
@@ -80,3 +80,175 @@
 // let result = a*b;
 // console.log(typeof result);
 
+// 5+5
+// "5"+5
+// "5"*5
+// 25*"abc"
+
+//scope
+// let a = 10;
+
+// if(true) {
+//     let b = 20;
+//     console.log(b);
+// }b
+// console.log(a);
+
+// // var (old,function scoped)
+// if (true) {
+//     var x = 10;
+//     var x = 20;
+// }
+// console.log(x); // (accesible outside the block)
+
+//hoisting
+
+// console.log(a);  //undifened
+// var a = 10;    
+
+// console.log(b);  
+// let b =20;
+
+// var z;
+// console.log(z);
+
+// loose (==) vs Strict (===) Equality
+
+// 5 == "5"   //true
+// true == 1 //true
+// null == undefined //true
+
+// 5 === "5"   //false
+// true === 1 //false
+// null === undefined //false
+
+// function fib(n){
+//     if(n==1){
+//         return 1;
+
+//     }
+//     if(n<=0){
+//         return 0;
+//     }
+//     return fib(n-1)+fib(n-2);
+// }
+
+// console.log(fib(10))
+
+// //IIFE function
+
+// (function(){
+//     console.log("I am running automatically");
+// })();
+
+// (function (name) {
+//     console.log("Hello " + name);
+
+// })
+// ("Swati");
+
+// (function(){
+
+// })();
+
+// //object
+
+// let user = {
+//     name: "Swati",
+//     age: 21,
+//     isAdmin: true
+// };
+
+// for(let key in user){
+//     console.log("key : ",key , "value :",user[key]);
+// }
+
+
+// console.log("USER", user);
+
+// let user2 = new Object();
+// user2.name = "SWati";
+// user2.age = 21;
+
+// console.log("USER",user2);
+
+// console.log("user 2 name", user2.name);
+
+// dot notation
+
+// console.log(user["name"]);
+
+// bracket notation
+// console.log(user["name"]);
+
+// objects with function and this keyword
+
+// let firstName = "Mumbai";
+// let person = {
+//     firstName: "Ananya",
+//     greet: function() {
+//         return "Hello " + this.firstName;
+//     }
+// };
+// console.log(person.greet());
+
+// // nested objects
+
+// let company = {
+//     name: "Google",
+//     location: {
+//         city: "Bangalore",
+//         pincode: 560001,
+//         googlemap:{
+//             latitude:" ",
+//             longitute:" "
+//         }
+//     }
+// };
+
+// console.log(company.location.pincode);
+
+// // Traversal of object in js
+
+// let company = {
+//     name: "Google",
+//     location: {
+//         city: "Bangalore",
+//         pincode: 560001,
+
+//     }
+// };
+
+// function printObject(company) {
+//     for (key in company) {
+//         if (typeof company[key] === 'object') {
+//             // for (let innerkey in company[key]) {
+//             //     console.log(innerkey, company[key][innerkey]);
+//             // }
+//             console.log("Object",company[key]);
+//             printObject(company[key]);
+//         }
+//         else {
+//             console.log(key, company[key]);
+//         }
+//     }                    
+// }
+
+// printObject(company);
+
+// // arrays
+
+// let a1 = [1,2,3];
+// let a2 = a1;
+
+// a2.push(4);
+// console.log(a1);  //[1,2,3,4]
+
+// //copy methods 
+// let b = a1;
+// b.push(55);
+// console.log(a1);
+
+// let c =[...a1];
+// c.push(90);
+// console.log(a1);
